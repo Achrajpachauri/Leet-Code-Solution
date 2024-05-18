ce697da1168899b1ee60fa02246d724705a6e25e
Achrajpachauri <achrajpachauri85@gmail.com>
2024-05-18 07:38:52 +0530

    2108-string

---
+++ 2108. Find First Palindromic String in the Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        
+        for i in words:
+            if i == i[len(i)-1::-1]:
+                return i
+        
+        return ""
