598075669f4bd2b48ebcfaaa8d38c704cf7c1d5d
Achrajpachauri <achrajpachauri85@gmail.com>
2024-06-12 22:47:05 +0530

    1002-string

---
+++ 1002. Find Common Characters.py
@@ -0,0 +1,14 @@
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+       
+        if len(words) == 1:
+            return words[0]
+
+        result = []
+        chars = set(words[0])
+        
+        for char in chars:
+            frequency = min([word.count(char) for word in words])
+            result += frequency * [char]
+
+        return result
